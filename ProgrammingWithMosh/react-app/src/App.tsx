f4e440f4be12dd397d3423f5a9885b7c5753380c
Igor Fernandes <igrfclima@gmail.com>
2025-07-14 20:08:49 -0300

    add: boostrap

--- ProgrammingWithMosh/react-app/src/App.tsx
+++ ProgrammingWithMosh/react-app/src/App.tsx
@@ -1,9 +1,9 @@
-import Message from "./Message";
+import ListGroup from "./components/ListGroup";
 
 function App() {
   return (
-    <div className="App">
-      <Message />
+    <div>
+      <ListGroup />
     </div>
   );
 }
