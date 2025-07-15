a889fa4a173b91caad9b53fafc10e82b4a9bf917
Igor Fernandes <igrfclima@gmail.com>
2025-07-15 09:18:38 -0300

    remove: unnecessary code from app

--- ProgrammingWithMosh/react-app/src/App.tsx
+++ ProgrammingWithMosh/react-app/src/App.tsx
@@ -1,9 +1,7 @@
 import Alert from "./components/Alert";
 import Button from "./components/Button";
-import ListGroup from "./components/ListGroup";
 
 function App() {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
   return (
     <div>
       <Alert>
