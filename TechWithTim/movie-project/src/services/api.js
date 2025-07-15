078f3b42665bdddc9e6b28d329e882fd5c861542
Igor Fernandes <igrfclima@gmail.com>
2025-07-15 12:13:26 -0300

    feat: api fetch

--- TechWithTim/movie-project/src/services/api.js
+++ TechWithTim/movie-project/src/services/api.js
@@ -6,7 +6,6 @@ export const getPopularMovies = async () => {
   const data = await response.json();
   return data.results;
 };
-
 export const searchMovies = async (query) => {
   const response = await fetch(
     `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
