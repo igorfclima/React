6c0bd26a847ebec121f3dc9d927a2aff08b2e34d
Igor Fernandes <igrfclima@gmail.com>
2025-07-14 21:39:30 -0300

    feat: usestate

--- ProgrammingWithMosh/react-app/src/components/ListGroup.tsx
+++ ProgrammingWithMosh/react-app/src/components/ListGroup.tsx
@@ -1,11 +1,24 @@
+import { useState } from "react";
+
 function ListGroup() {
-  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
   return (
     <>
       <h1>List Group</h1>
       <ul className="list-group">
-        {items.map((item) => (
-          <li className="list-group-item" key={item}>
+        {items.length === 0 && <p>No items!</p>}
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => setSelectedIndex(index)}
+          >
             {item}
           </li>
         ))}
